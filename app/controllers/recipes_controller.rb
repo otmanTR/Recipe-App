9160d020dec82723de6710ab5e7b8d134c90205f
otmanTR <r.brk.otman@gmail.com>
2023-05-30 15:39:52 +0300

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,44 +1,38 @@
 class RecipesController < ApplicationController
-    
-    def index;
+  def index
     @user = current_user
     @recipes = @user.recipes
-    end
-  
+  end
 
-    def show
-        @recipe = Recipe.find(params[:id])
-        @user = User.find(params[:user_id])
-      end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 
-      def new
-        @recipe = Recipe.new
-      end
+  def new
+    @recipe = Recipe.new
+  end
 
-      def create
-        @recipe = current_user.recipes.new(recipe_params)
-        @recipe.user = current_user
-    
-        if @recipe.save
-    
-          redirect_to recipe_path(@recipe.id), notice: 'Recipe created successfully!'
-        else
-          notice: 'Recipe could not be created.'
-          render :new
-        end
-      end
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+    @recipe.user = current_user
 
-      def destroy
-        @recipe = Recipe.find(params[:id])
-        @recipe.destroy
-        redirect_to recipes_path
-      end
-    
+    if @recipe.save
 
-    private
-
-    def recipe_params
-      params.require(:recipe).permit(:name, :preperation_time, :cooking_time, :description, :public)
+      redirect_to recipe_path(@recipe.id), notice: 'Recipe created successfully!'
+    else
+      render :new
     end
   end
-  
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preperation_time, :cooking_time, :description, :public)
+  end
+end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,8 @@
 class Recipe < ApplicationRecord
-       belongs_to :user
+  belongs_to :user
 
-       validates :name, presence: true, length: { maximum: 250 }
-       validates :description, presence: true, length: { maximum: 500 }
-  
-    has_many :recipe_food, foreign_key: :user_id
-  
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :description, presence: true, length: { maximum: 500 }
 
-  end
+  has_many :recipe_food, foreign_key: :user_id
+end
